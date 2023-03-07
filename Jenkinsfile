96889744b0826d4657377a87201146481b7078cc
Eric Coughlin <ecoughlin@lsst.org>
2023-03-07 07:54:25 -0300

    Test pre-commit-conf

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,2 +1,2 @@
-@Library('JenkinsShared')_
-DevelopPipeline("ts-cbp", "CBP", "lsst.ts.cbp", [], false, [])
+@Library('JenkinsShared@u/couger01/add-pre-commit-conf')_
+DevelopPipeline("ts-cbp", "CBP", "lsst.ts.cbp", "--no-mypy", [], false, [])
--- doc/conf.py
+++ doc/conf.py
@@ -1,15 +1,15 @@
 """Sphinx configuration file for TSSW package"""
 
-from documenteer.conf.pipelinespkg import *
+from documenteer.conf.pipelinespkg import *  # noqa
 
 project = "ts_cbp"
-html_theme_options["logotext"] = project
+html_theme_options["logotext"] = project  # noqa
 html_title = project
 html_short_title = project
 
 intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
 intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # noqa
-intersphinx_mapping["ts_simactuators"] = (
+intersphinx_mapping["ts_simactuators"] = (  # noqa
     "https://ts-simactuators.lsst.io",
     None,
 )  # noqa
